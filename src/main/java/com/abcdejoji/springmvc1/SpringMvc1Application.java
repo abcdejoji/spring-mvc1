cfa80716bf917f1706a3ca796fa3762182a03390
joji <abcdejoji@gmail.com>
2025-11-14 06:58:58 +0900

    feat: hello servlet

--- src/main/java/com/abcdejoji/springmvc1/SpringMvc1Application.java
+++ src/main/java/com/abcdejoji/springmvc1/SpringMvc1Application.java
@@ -2,7 +2,9 @@ package com.abcdejoji.springmvc1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.*;
 
+@ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
 public class SpringMvc1Application {
 
